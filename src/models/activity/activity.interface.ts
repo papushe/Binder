50614c0544c585d6f7e36333f214115e5e3c29a1
tomerktzv <tomerktzv@gmail.com>
2018-04-03 01:12:33 +0300

    added activity interface

--- src/models/activity/activity.interface.ts
+++ src/models/activity/activity.interface.ts
@@ -1,3 +1,21 @@
-/**
- * Created by tomerktzv on 02/04/2018.
- */
+export interface Activity {
+  _id: string,
+  activity_name: string,
+  activity_description: string,
+  type: string,
+  created_at: string,
+  consumer_id: string,
+  provider_id: string,
+  community_id: string,
+  notes: string,
+  source: {
+    city: string,
+    street: string,
+    number: string
+  },
+  destination: {
+    city: string,
+    street: string,
+    number: string
+  }
+}
