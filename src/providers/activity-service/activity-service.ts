2969f831e82c0f6155dbfcf0058a18667c110046
tomerktzv <tomerktzv@gmail.com>
2018-04-14 15:41:09 +0300

    added getActivitiesByCommunityId

--- src/providers/activity-service/activity-service.ts
+++ src/providers/activity-service/activity-service.ts
@@ -43,4 +43,10 @@ export class ActivityServiceProvider {
       .post(`${this.baseUrl}/deleteActivityById/`, activityIdObj)
   }
 
+
+  getActivitiesByCommunityId(activity: Activity) {
+    return this._http
+      .get(`${this.baseUrl}/getActivitiesByCommunityId/${activity._id}`)
+  }
+
 }
