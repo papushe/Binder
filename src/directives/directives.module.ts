fa367e5666184596f6f140bc019c790411428b7d
Papushe <papushe9@gmail.com>
2018-04-19 19:27:11 +0300

    new color directive

---
+++ src/directives/directives.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { paintAfterChange } from './paintAfterChange/paintAfterChange';
+@NgModule({
+	declarations: [paintAfterChange],
+	imports: [],
+	exports: [paintAfterChange]
+})
+export class DirectivesModule {}
