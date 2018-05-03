650d168616ea6c639211e2818154efc51eb199eb
NaorHai <Na1994or>
2018-05-03 12:01:37 +0300

    notification service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,6 +27,7 @@ import {HTTP_INTERCEPTORS} from "@angular/common/http";
 import {SocketService} from '../providers/socket/socket.service';
 import {ChatService} from '../providers/chat-service/chat-service';
 import { NotitficationProvider } from '../providers/notitfication/notification';
+import { MessageService } from '../providers/message/message';
 
 const config: SocketIoConfig = {url: 'http://localhost:4300', options: {}};
 
@@ -70,7 +71,8 @@ const config: SocketIoConfig = {url: 'http://localhost:4300', options: {}};
       multi: true
     },
     ChatService,
-    NotitficationProvider
+    NotitficationProvider,
+    MessageService
   ]
 })
 export class AppModule {
