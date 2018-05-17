55c124f086f93d709cfb57dcd599f5c95c563ae9
Papushe <papushe9@gmail.com>
2018-05-17 08:52:25 +0300

    refactoring

--- src/pages/member-options/member-options.ts
+++ src/pages/member-options/member-options.ts
@@ -1,35 +1,32 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {IonicPage, NavController, NavParams} from 'ionic-angular';
 import {Profile} from "../../models/profile/profile.interface";
 import {Community} from "../../models/community/community.interface";
-
-/**
- * Generated class for the MemberOptionsPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @IonicPage()
 @Component({
   selector: 'page-member-options',
   templateUrl: 'member-options.html',
 })
-export class MemberOptionsPage{
+export class MemberOptionsPage implements OnInit {
 
   member: Profile;
   community: Community;
   isJoined: boolean;
   title: string;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-    this.member = navParams.get('member');
-    this.community = navParams.get('community');
-    this.isJoined = navParams.get('isJoined');
-    this.title = this.member ? `${this.member.firstName} ${this.member.lastName}` : `User Options`;
+  constructor(private navCtrl: NavController, private navParams: NavParams) {
   }
 
-  ionViewDidLoad() {
+  ngOnInit() {
+    this.init();
+  }
+
+  init() {
+    this.member = this.navParams.get('member');
+    this.community = this.navParams.get('community');
+    this.isJoined = this.navParams.get('isJoined');
+    this.title = this.member ? `${this.member.firstName} ${this.member.lastName}` : `User Options`;
   }
 
 }
