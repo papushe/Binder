b4e89b2b5470657bbabc27784de4d1c968a90e98
Papushe <papushe9@gmail.com>
2018-05-17 08:47:57 +0300

    refactoring

--- src/providers/activity-service/activity-service.ts
+++ src/providers/activity-service/activity-service.ts
@@ -2,12 +2,6 @@ import {HttpClient} from '@angular/common/http';
 import {Injectable} from '@angular/core';
 import {Activity} from '../../models/activity/activity.interface';
 
-/*
-  Generated class for the ActivityService provider.
-
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
 @Injectable()
 export class ActivityService {
 
