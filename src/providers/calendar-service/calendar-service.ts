04c3139ee6e1e13ee3051023faf4df7ccf20aa00
NaorHai <Na1994or>
2018-06-02 10:59:40 +0300

    delete calendar event

--- src/providers/calendar-service/calendar-service.ts
+++ src/providers/calendar-service/calendar-service.ts
@@ -7,6 +7,7 @@ export class CalendarService {
 
   permission: boolean;
   activity: Activity;
+  hour = 60 * 60 * 1000;
 
   constructor(private calendar: Calendar) {
     this.calendar.hasReadPermission()
@@ -35,9 +36,6 @@ export class CalendarService {
       this.askForPermission();
     }
 
-    let hour = 60 * 60 * 1000;
-    let activityLocalDate = new Date(activity.activity_date);
-
     let options = {
       id: activity._id,
       firstReminderMinutes: 120,
@@ -49,8 +47,8 @@ export class CalendarService {
       title: activity.activity_name,
       location: activity.source,
       notes: activity.notes || '',
-      startDate: new Date(activity.activity_date ),
-      endDate: new Date(activity.activity_date + hour)
+      startDate: new Date(activity.activity_date),
+      endDate: new Date(activity.activity_date + this.hour)
     };
 
     this.calendar.createEventInteractivelyWithOptions(event.title, event.location, event.notes, event.startDate, event.endDate, options)
@@ -59,6 +57,16 @@ export class CalendarService {
       })
       .catch(err => {
         console.error(`failed to create a new event with error: ${err}`);
+      });
+  }
+
+  deleteEvent(activity: Activity) {
+    this.calendar.deleteEvent(activity.activity_name, activity.source, activity.notes || '', new Date(activity.activity_date), new Date(activity.activity_date + this.hour))
+      .then(data => {
+        console.log(`new event was removed from user calendar ${data}`);
+      })
+      .catch(err => {
+        console.error(`failed to delete event with error: ${err}`);
       });
   }
 
